c614031af427b9a59733178e7c43950724320a22
Muhammad-ahmadi <Muuhammad.ahmadi@gmail.com>
2019-11-03 17:51:06 +0330

    Register vee-validate globally with full list of validation rules

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { ValidationProvider } from "vee-validate/dist/vee-validate.full";
+// import { ValidationProvider, extend } from "vee-validate";
 
+Vue.component("ValidationProvider", ValidationProvider);
 Vue.config.productionTip = false;
 
 new Vue({
